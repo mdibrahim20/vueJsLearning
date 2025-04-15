3d46bed44b748625f75c38f058704149322ffdc9
Md Ibrahim <imuhammad784@gmail.com>
2025-04-15 18:18:55 +0600

    Simple Project

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,42 +1,38 @@
 <template>
-  <h1>HomePage</h1>
-
-  <div>
-    <h1 :class="{green:colorful}">Class Binding</h1>
-    <button v-on:click="colorful=!colorful">Click Me</button>
-  </div>
+<div>
+  <h1>User Cards</h1>
+  <UserCard v-for="(user) in users" :key="user.email" :name="user.name" :email="user.email" :isActive="user.active" @user-clicked="handleUserClick"/>
+  <h3>{{ message }}</h3>
+</div>
 
 </template>
 
 <script>
+import UserCard from './UserCard.vue';
 export default {
   name: "HomePage",
-  data() {
-    return {
-      colorful:true,
+  components:{
+    UserCard
+  },
+  data(){
+    return{
+      users: [
+        { name: 'Ibrahim', email: 'ibrahim@vue.com', active: true },
+        { name: 'Zanin', email: 'zanin@vue.com', active: false },
+        { name: 'Araf', email: 'araf@vue.com', active: true },
+      ],
+      message:""
+    }
+  },
+  methods:{
+    handleUserClick(msg){
+      this.message = msg;
     }
   }
+
 }
 </script>
 
 <style scoped>
-h1,
-h2 {
-  color: red;
-}
-h2 {
-  font-size: 50px;
-}
 
-.green{
-  background-color: green;
-  padding: 20px;
-  width: 200px;
-  height: 50px;
-  border-radius: 10px;
-}
-
-.red{
-  color: red;
-}
 </style>
