033e9471f21889cc53fc9ace00cbf36aabd0cbb6
Md Ibrahim <imuhammad784@gmail.com>
2025-04-15 17:51:22 +0600

    single and multi html binding

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,46 +1,22 @@
 <template>
   <h1>HomePage</h1>
-  
-  <ul>
-    <li v-for="items in getContact" :key="items.name">
-      <Users :data="items" :getPhone="getData"/>
-    </li>
-  </ul>
+  <h3>{{ text }}</h3>
+  <div v-html="html1 + html2">
+  </div>
+
 </template>
 
 <script>
-import Users from "./Users.vue";
 export default {
   name: "HomePage",
-  components: {
-    Users,
-  },
   data() {
     return {
-      userData: {
-        name: "Araf",
-        email: "Araf@gmail.com",
-      },
-      getContact: [
-        { name: "Araf", phone: "01711111111" },
-        { name: "Zara", phone: "01712222222" },
-        { name: "Imran", phone: "01713333333" },
-        { name: "Lamia", phone: "01714444444" },
-        { name: "Tanvir", phone: "01715555555" },
-        { name: "Sadia", phone: "01716666666" },
-        { name: "Rafi", phone: "01717777777" },
-        { name: "Nadia", phone: "01718888888" },
-        { name: "Hasan", phone: "01719999999" },
-        { name: "Mira", phone: "01710000000" },
-      ],
-    };
-  },
-  methods: {
-    getData(phone) {
-      alert(`phone number is ${phone}`);
-    },
-  },
-};
+      text:"Md Ibrahim",
+      html1:"<h1>Md Ibrahim Khalil Ullah</h1>",
+      html2:"<h1>Araf Ahmed</h1>",
+    }
+  }
+}
 </script>
 
 <style scoped>
