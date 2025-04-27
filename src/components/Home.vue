23cae07a0e9aa12c557d4004fb545b22305ab013
Md Ibrahim <imuhammad784@gmail.com>
2025-04-27 19:15:26 +0600

    ref uses

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,38 +1,23 @@
 <template>
-<div>
-  <h1>User Cards</h1>
-  <UserCard v-for="(user) in users" :key="user.email" :name="user.name" :email="user.email" :isActive="user.active" @user-clicked="handleUserClick"/>
-  <h3>{{ message }}</h3>
-</div>
+
+    <h1>Ref uses vue</h1>
+    <input type="text" ref="input" />
+    <button v-on:click="getData">Click Me</button>
 
 </template>
 
 <script>
-import UserCard from './UserCard.vue';
 export default {
   name: "HomePage",
-  components:{
-    UserCard
-  },
-  data(){
-    return{
-      users: [
-        { name: 'Ibrahim', email: 'ibrahim@vue.com', active: true },
-        { name: 'Zanin', email: 'zanin@vue.com', active: false },
-        { name: 'Araf', email: 'araf@vue.com', active: true },
-      ],
-      message:""
+  methods: {
+    getData() {
+      this.$refs.input.focus();
+      let val = this.$refs.input.value;
+      console.log(val)
+      this.$refs.input.style.color='red';
     }
   },
-  methods:{
-    handleUserClick(msg){
-      this.message = msg;
-    }
-  }
-
-}
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
