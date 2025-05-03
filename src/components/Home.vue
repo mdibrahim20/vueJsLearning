025eea70dcdf9fd44b8520d502639e63125e1344
Md Ibrahim <imuhammad784@gmail.com>
2025-05-04 01:37:56 +0600

    Props and non-props

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,14 +2,17 @@
   <h1>Ref uses vue</h1>
   <input type="text" ref="input" />
   <Form></Form>
+  <UserComp data="Ibrahim Khalil" id="test-id"></UserComp>
 </template>
 
 <script>
 import Form from "./FormCompo";
+import UserComp from "./User.vue";
 export default {
   name: "HomePage",
   components: {
     Form,
+    UserComp
   },
 };
 </script>
---
+++ src/components/User.vue
@@ -0,0 +1,18 @@
+<template>
+  <div v-bind="$attrs">
+    <h1>Ibrahim</h1>
+    <h1>Araf</h1>
+    <h1>Masud</h1>
+    <h1>{{ data }}</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "UserComp",
+  props: {
+    data:String,
+    id:String,
+  },
+};
+</script>
+<style scoped></style>
