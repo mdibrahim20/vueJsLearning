d2b5939d83964dbbdf1d283597922014a02b8eb2
Md Ibrahim <imuhammad784@gmail.com>
2025-05-04 01:44:33 +0600

    Computed Property

--- src/components/User.vue
+++ src/components/User.vue
@@ -4,6 +4,8 @@
     <h1>Araf</h1>
     <h1>Masud</h1>
     <h1>{{ data }}</h1>
+
+    <h1>Total: {{getResult}} </h1>
   </div>
 </template>
 <script>
@@ -13,6 +15,21 @@ export default {
     data:String,
     id:String,
   },
+  data(){
+    return{
+        dollar:120,
+        takaVal:100,
+        discount:35
+
+    }
+  },
+  computed:{
+    getResult(){
+        return (this.dollar*this.takaVal)-this.discount;
+    }
+  }
 };
 </script>
 <style scoped></style>
+
+<!-- Computed property: simply for cache works. like if a value will not change for a long time then its being used. when the value will be updated in that case it will automatically re-cache the original one -->
