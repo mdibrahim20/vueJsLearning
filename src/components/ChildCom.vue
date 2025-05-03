752e3084f65f3d7cbbde9bf090f2e25d0c8555a2
Md Ibrahim <imuhammad784@gmail.com>
2025-05-04 03:07:08 +0600

    Multi-Named-slots

--- src/components/ChildCom.vue
+++ src/components/ChildCom.vue
@@ -1,9 +1,27 @@
 <template>
-<slot>hello</slot>
+  <div class="cart">
+    <header>
+    <slot name="header"></slot>
+  </header>
+  <content>
+    <slot name="main"></slot>
+  </content>
+  <footer>
+    <slot name="footer"><h1>Footer</h1></slot>
+  </footer>
+  </div>
 </template>
 <script>
 export default {
-    name:"ChildCom",
-}
+  name: "ChildCom",
+};
 </script>
-<style scoped></style>
+<style scoped>
+.cart{
+    border: 1px solid red;
+    width: 400px;
+    text-align: center;
+    margin-bottom: 20px;
+    border-radius: 5px;
+}
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,18 +1,48 @@
 <template>
-  <ChildComp><h1>Ibrahim</h1></ChildComp>
-  <ChildComp><a href="#">www.google.com</a></ChildComp>
-  <ChildComp><img src="https://thafd.bing.com/th/id/OIP.0iqvqUM-_MntTZp4CMBaigHaEK?cb=iwp1&rs=1&pid=ImgDetMain"/></ChildComp>
-  <ChildComp></ChildComp>
+  <ChildComp>
+    <template v-slot:header>
+      <h1>Google</h1>
+    </template>
+    <template v-slot:main>
+      <img src="https://thafd.bing.com/th/id/OIP.URPfXjbxMgRsxwxpTdBm5AHaG0?cb=iwp1&rs=1&pid=ImgDetMain" width="200px"/>
+      <h1>this is main / content part</h1>
+    </template>
+    <template v-slot:footer>
+      <h1>This is footer</h1>
+    </template>
+  </ChildComp>
+  <ChildComp>
+    <template v-slot:header>
+      <h1>Youtube</h1>
+    </template>
+    <template v-slot:main>
+      <img src="https://thafd.bing.com/th/id/OIP.URPfXjbxMgRsxwxpTdBm5AHaG0?cb=iwp1&rs=1&pid=ImgDetMain" width="200px"/>
+      <h1>this is main / content part</h1>
+    </template>
+    <template v-slot:footer>
+      <h1>This is footer</h1>
+    </template>
+  </ChildComp>
+  <ChildComp>
+    <template v-slot:header>
+      <h1>Facebook</h1>
+    </template>
+    <template v-slot:main>
+      <img src="https://thafd.bing.com/th/id/OIP.URPfXjbxMgRsxwxpTdBm5AHaG0?cb=iwp1&rs=1&pid=ImgDetMain" width="100px"/>
+      <h1>this is main / content part</h1>
+    </template>
+    <template v-slot:footer>
+  
+    </template>
+  </ChildComp>
 </template>
 
 <script>
-
 import ChildComp from "./ChildCom.vue";
 export default {
   name: "HomePage",
   components: {
-
-    ChildComp
+    ChildComp,
   },
 };
 </script>
